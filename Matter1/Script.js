687461a2f926dcba2bba1bd110d193914e0db044
shivamsetha2d <153591512+shivamsetha2d@users.noreply.github.com>
2023-12-23 01:52:11 +0530

    matter 1 is fineshed

--- Matter1/Script.js
+++ Matter1/Script.js
@@ -1,3 +1,36 @@
-var a = 12;
-console.log(a) 
-// write node ./script.js
+// var a = 12;
+// console.log(a) 
+// write node ./script.js to run js code via node 
+
+
+// var x = require('./script2');
+
+// console.log(x);
+
+
+// var oneLinerJoke = require('one-liner-joke');
+
+// var getRandomJoke = oneLinerJoke.getRandomJoke();
+// console.log(getRandomJoke)
+
+
+const express = require('express')
+const app = express()
+
+app.use(function(res, req, next){
+    console.log("from middleware");
+    next();
+})
+
+app.use(function(res, req, next){
+    console.log("from middleware 2");
+    next();
+})
+
+app.get('/', function (req, res) {
+  res.send('shivam')
+})
+app.get('/profile', function (req, res) {
+    res.send('profile ')
+  })
+app.listen(3000)
